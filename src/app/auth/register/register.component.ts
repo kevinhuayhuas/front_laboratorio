79542394ec79404454006ad9e9934aab7e3f1867
kevin huayhuas <kevin.huayhuasgmail.com>
2024-07-10 16:40:25 -0500

    registro

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -7,18 +7,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit {
-
-
   registerForm : FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
+      nombres: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
-    });
+      password: ['', [Validators.required]],
+      confirmPassword: ['', Validators.required]
+    },{ validators: this.passwordsMatch });
   }
-  onSubmit(){
-
+  passwordsMatch(group: FormGroup) {
+    const password = group.get('password')?.value || '';
+    const confirmPassword = group.get('confirmPassword')?.value || '';
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulario válido', this.registerForm.value);
+    } else {
+      console.log('Formulario inválido');
+    }
   }
   ngOnInit(): void {}
 
